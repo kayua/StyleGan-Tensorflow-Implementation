8c50d708c694042580603dd6ce91bf4b4dc142fe
kayua <kayuaolequesp@gmail.com>
2022-06-25 19:47:37 -0300

    Update

--- main.py
+++ main.py
@@ -1,16 +1,65 @@
-# This is a sample Python script.
+import numpy
+import tensorflow
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Models.StyleGan.StyleGan import StyleGAN
+
+START_RES = 4
+TARGET_RES = 128
+
+style_gan = StyleGAN(start_res=START_RES, target_res=TARGET_RES)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def train(
+    start_res=START_RES,
+    target_res=TARGET_RES,
+    steps_per_epoch=5000,
+    display_images=True,
+):
+    opt_cfg = {"learning_rate": 1e-3, "beta_1": 0.0, "beta_2": 0.99, "epsilon": 1e-8}
+
+    val_batch_size = 16
+    val_z = tensorflow.random.normal((val_batch_size, style_gan.z_dim))
+    val_noise = style_gan.generate_noise(val_batch_size)
+
+    start_res_log2 = int(numpy.log2(start_res))
+    target_res_log2 = int(numpy.log2(target_res))
+
+    for res_log2 in range(start_res_log2, target_res_log2 + 1):
+        res = 2 ** res_log2
+        for phase in ["TRANSITION", "STABLE"]:
+            if res == start_res and phase == "TRANSITION":
+                continue
+
+            train_dl = create_dataloader(res)
+
+            steps = int(train_step_ratio[res_log2] * steps_per_epoch)
+
+            style_gan.compile(
+                d_optimizer=tensorflow.keras.optimizers.Adam(**opt_cfg),
+                g_optimizer=tensorflow.keras.optimizers.Adam(**opt_cfg),
+                loss_weights={"gradient_penalty": 10, "drift": 0.001},
+                steps_per_epoch=steps,
+                res=res,
+                phase=phase,
+                run_eagerly=False,
+            )
+
+            prefix = f"res_{res}x{res}_{style_gan.phase}"
+
+            ckpt_cb = tensorflow.keras.callbacks.ModelCheckpoint(
+                f"checkpoints/stylegan_{res}x{res}.ckpt",
+                save_weights_only=True,
+                verbose=0,
+            )
+            print(phase)
+            style_gan.fit(
+                train_dl, epochs=1, steps_per_epoch=steps, callbacks=[ckpt_cb]
+            )
+
+            if display_images:
+                images = style_gan({"z": val_z, "noise": val_noise, "alpha": 1.0})
+                plot_images(images, res_log2)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+train(start_res=4, target_res=16, steps_per_epoch=1, display_images=False)
