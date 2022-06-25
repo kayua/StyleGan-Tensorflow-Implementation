75cb6d37492cbe9430a3029745eb912e1a2773f8
kayua <kayuaolequesp@gmail.com>
2022-06-25 19:07:11 -0300

    Update

---
+++ Models/Layers/EqualizedDense.py
@@ -0,0 +1,32 @@
+import tensorflow
+from keras import layers
+from tensorflow import keras
+
+
+class EqualizedDense(layers.Layer):
+    def __init__(self, units, gain=2, learning_rate_multiplier=1, **kwargs):
+        super(EqualizedDense, self).__init__(**kwargs)
+        self.units = units
+        self.gain = gain
+        self.learning_rate_multiplier = learning_rate_multiplier
+
+    def build(self, input_shape):
+        self.in_channels = input_shape[-1]
+        initializer = keras.initializers.RandomNormal(
+            mean=0.0, stddev=1.0 / self.learning_rate_multiplier
+        )
+        self.w = self.add_weight(
+            shape=[self.in_channels, self.units],
+            initializer=initializer,
+            trainable=True,
+            name="kernel",
+        )
+        self.b = self.add_weight(
+            shape=(self.units,), initializer="zeros", trainable=True, name="bias"
+        )
+        fan_in = self.in_channels
+        self.scale = tensorflow.sqrt(self.gain / fan_in)
+
+    def call(self, inputs):
+        output = tensorflow.add(tensorflow.matmul(inputs, self.scale * self.w), self.b)
+        return output * self.learning_rate_multiplier
