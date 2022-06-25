47fbf2011e418f0498961c86d932b456f668f341
kayua <kayuaolequesp@gmail.com>
2022-06-25 19:18:55 -0300

    Update

--- Models/functions/functions.py
+++ Models/functions/functions.py
@@ -1,4 +1,7 @@
 import tensorflow
+from keras import layers
+
+from Models.Layers.EqualizedDense import EqualizedDense
 
 
 def fade_in(alpha, a, b):
@@ -22,3 +25,12 @@ def minibatch_std(input_tensor, epsilon=1e-8):
     avg_std = tensorflow.reduce_mean(group_std, axis=[1, 2, 3], keepdims=True)
     x = tensorflow.tile(avg_std, [group_size, h, w, 1])
     return tensorflow.concat([input_tensor, x], axis=-1)
+
+def Mapping(num_stages, input_shape=512):
+    z = layers.Input(shape=(input_shape))
+    w = pixel_norm(z)
+    for i in range(8):
+        w = EqualizedDense(512, learning_rate_multiplier=0.01)(w)
+        w = layers.LeakyReLU(0.2)(w)
+    w = tensorflow.tile(tensorflow.expand_dims(w, 1), (1, num_stages, 1))
+    return tensorflow.keras.Model(z, w, name="mapping")
