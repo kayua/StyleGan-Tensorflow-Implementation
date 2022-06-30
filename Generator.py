87fd53ffeacbee30f14a3edc6c888057ab1248e7
kayua <kayuaolequesp@gmail.com>
2022-06-30 17:11:59 -0300

    Update

--- Generator.py
+++ Generator.py
@@ -4,9 +4,8 @@ from keras.layers import Dense, LeakyReLU, Reshape, Conv2D, UpSampling2D
 from Layers.AdaIN import AdaIN
 from Layers.AddNoise import AddNoise
 
-
-
 level_size_feature_dimension = [4, 8, 16, 32, 64]
+
 
 class Generator:
 
@@ -24,15 +23,12 @@ class Generator:
         self.input_block = None
         self.list_level_block_output = []
         self.list_level_noise_input = []
+        self.function_loss = "binary_crossentropy"
 
         self.num_filters_per_level = [32, 32, 32, 32]
 
-    def build_blocks(self):
+    def block_mapping_network(self):
         latent_dimension_input = Input(shape=(self.latent_dimension, 1))
-        self.mapping_neural_network = self.__block_mapping_network(latent_dimension_input)
-
-    def __block_mapping_network(self, latent_dimension_input):
-
         if self.num_mapping_blocks > 1:
 
             gradient_flow = Dense(self.num_neurons_mapping)(latent_dimension_input)
@@ -44,7 +40,7 @@ class Generator:
             gradient_flow = Dense(self.latent_dimension)(gradient_flow)
             network_model = Model(latent_dimension_input, gradient_flow, name="Mapping_Network")
             network_model.summary()
-            return network_model
+            self.mapping_neural_network = network_model
 
     def constant_mapping_block(self):
 
@@ -75,59 +71,36 @@ class Generator:
         gradient_flow = AddNoise()([gradient_flow, input_noise])
         gradient_flow = AdaIN()([gradient_flow, input_latent])
 
-        gradient_flow = Model([input_flow, input_noise, input_latent], gradient_flow, name="Synthesis_Block_{}".format(resolution_block))
-        gradient_flow.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        gradient_flow = Model([input_flow, input_noise, input_latent], gradient_flow)
+
+        gradient_flow.compile(loss=self.function_loss, optimizer='adam', metrics=['accuracy'])
         gradient_flow.summary()
         return gradient_flow
 
     def build_synthesis_block(self):
 
-
         input_flow = Input(shape=(self.initial_dimension, self.initial_dimension, self.initial_num_channels))
 
         input_latent = Input(shape=(self.latent_dimension, 1))
 
+        if self.num_synthesis_block <= 1: return -1
 
-        if self.num_synthesis_block <= 1:
-
-            return -1
         input_noise = Input(shape=(self.initial_dimension, self.initial_dimension, self.initial_num_channels))
         first_level_block = self.block_synthesis(4, self.initial_num_channels, True)
         first_level_block = first_level_block([input_flow, input_noise, input_latent])
         self.list_level_block_output.append(first_level_block)
         self.list_level_noise_input.append(input_noise)
 
-
-        for i in range(self.num_synthesis_block-1):
-
-
-            input_noise = Input(shape=(level_size_feature_dimension[i+1], level_size_feature_dimension[i+1], self.initial_num_channels))
-            print(level_size_feature_dimension[i+1])
+        for i in range(self.num_synthesis_block - 1):
+            resolution_feature = level_size_feature_dimension[i + 1]
+            input_noise = Input(shape=(resolution_feature, resolution_feature, self.initial_num_channels))
             self.list_level_noise_input.append(input_noise)
             level_block = self.block_synthesis(level_size_feature_dimension[i], self.initial_num_channels, False)
             level_block = level_block([self.list_level_block_output[-1], self.list_level_noise_input[-1], input_latent])
             self.list_level_block_output.append(level_block)
 
 
-        #network_model = Model([input_flow, input_noise, input_latent], [self.list_level_block_output[-1]])
-        #network_model.summary()
-        #input_flow = Input(shape=(4, 4, self.num_filters_per_level[0]))
-        #input_noise = Input(shape=(4, 4, self.num_filters_per_level[0]))
-        #input_latent = Input(shape=(self.latent_dimension, 1))
-        #first_level_block = Model(first_level_block([input_flow, input_noise, input_latent]), first_level_block.output)
-       # first_level_block.summary()
 
-
-        #first_level_block = UpSampling2D()(first_level_block.output)
-
-        #self.list_level_block_input.append(first_level_block)
-        #last_resolution_block = self.num_mapping_blocks ** 2
-        #for i in range(self.num_synthesis_block):
-        #    last_level_block = self.list_level_block_input[-1]
-        #    last_resolution_block = last_resolution_block ** 2
-        #    level_block = self.block_synthesis(last_resolution_block, self.num_filters_per_level[i - 1])(last_level_block)
-        #    level_block = UpSampling2D()(level_block.output)
-        #    self.list_level_block_input.append(level_block)
 
 
 
