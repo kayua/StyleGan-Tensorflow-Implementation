8cfd8cce5795a5e9c19c2e1a54ed55949f4a87b1
kayua <kayuaolequesp@gmail.com>
2022-07-03 17:28:16 -0300

    Update

--- Layers/AddNoise.py
+++ Layers/AddNoise.py
@@ -4,6 +4,11 @@ from tensorflow.python.layers.base import Layer
 
 
 class AddNoise(Layer):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.b = None
+
     def build(self, input_shape):
         n, h, w, c = input_shape[0]
         initializer = keras.initializers.RandomNormal(mean=0.0, stddev=1.0)
@@ -11,7 +16,7 @@ class AddNoise(Layer):
             shape=[1, 1, 1, c], initializer=initializer, trainable=True, name="kernel"
         )
 
-    def call(self, inputs):
+    def call(self, inputs, **kwargs):
         x, noise = inputs
         output = x + self.b * noise
         return output
