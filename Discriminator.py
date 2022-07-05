78f5bafa2ae5cd514f0e2d2359a7aa4798313d8f
kayua <kayuaolequesp@gmail.com>
2022-07-05 18:39:06 -0300

    Update

--- Discriminator.py
+++ Discriminator.py
@@ -1,5 +1,5 @@
 from keras import Input, Model
-from keras.layers import Conv2D, LeakyReLU, MaxPooling2D
+from keras.layers import Conv2D, LeakyReLU, MaxPooling2D, Dense, Normalization
 
 level_size_feature_dimension = [512, 256, 128, 64, 32, 16, 8, 4, 4]
 number_filters_per_layer = [4, 8, 16, 32, 64, 128, 256, 512, 512]
@@ -15,6 +15,7 @@ class Discriminator:
         self.optimizer_function = "adam"
         self.input_discriminator = []
         self.discriminator_blocks = []
+        self.first_level_discriminator = None
         pass
 
 
@@ -33,10 +34,28 @@ class Discriminator:
         gradient_flow.summary()
 
     def build_discriminator(self):
+        resolution_feature = level_size_feature_dimension[-1]
+        input_layer = Input(shape=(resolution_feature, resolution_feature, number_channels))
+        number_layer = number_filters_per_layer[-1]
+        self.first_level_discriminator = Conv2D(number_layer, (3, 3), padding="same")(input_layer)
+        self.first_level_discriminator = Conv2D(number_layer, (3, 3), padding="same")(self.first_level_discriminator)
+        self.first_level_discriminator = self.fully_connected_block(self.first_level_discriminator)
+        self.first_level_discriminator = Model(input_layer, self.first_level_discriminator)
+        self.first_level_discriminator.summary()
 
         for i in range(len(level_size_feature_dimension)):
 
             self.convolutional_block(level_size_feature_dimension[i], number_filters_per_layer[i])
+
+
+    @staticmethod
+    def fully_connected_block(input_layer):
+        gradient_flow = Normalization(input_layer)
+        gradient_flow = Dense(1)(gradient_flow)
+        gradient_flow = LeakyReLU(0.2)(gradient_flow)
+        return gradient_flow
+
+
 
 
 
