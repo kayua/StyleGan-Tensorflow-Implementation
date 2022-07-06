19e940cf1e706f6d7a9aff439e0e5ddd52377cc0
kayua <kayuaolequesp@gmail.com>
2022-07-06 08:32:01 -0300

    Update

--- Neural.py
+++ Neural.py
@@ -1,10 +1,9 @@
 import tensorflow
 from tensorflow import keras
 
-from Discriminator import Discriminator
-from Generator import Generator
-from StyleGAN import StyleGAN
-
+from Engine.Discriminator import Discriminator
+from Engine.Generator import Generator
+from Engine.StyleGAN import StyleGAN
 from Tools.LoadImage import LoadImage
 
 generator_optimizer = keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5, beta_2=0.9)
@@ -20,7 +19,7 @@ def discriminator_loss(real_img, fake_img):
 def generator_loss(fake_img):
     return -tensorflow.reduce_mean(fake_img)
 
-level = 3
+level = 2
 discriminator_instance = Discriminator()
 generator_instance = Generator()
 image_instance = LoadImage()
@@ -33,3 +32,4 @@ discriminator_model = discriminator_instance.get_discriminator(level)
 
 styleGan = StyleGAN(discriminator=discriminator_model, generator=generator_model, latent_dim=0, discriminator_extra_steps=3, level_network=level)
 styleGan.compile(d_optimizer=discriminator_optimizer, g_optimizer=generator_optimizer, g_loss_fn=generator_loss, d_loss_fn=discriminator_loss)
+styleGan.fit(image_training, batch_size=32, epochs=10)
